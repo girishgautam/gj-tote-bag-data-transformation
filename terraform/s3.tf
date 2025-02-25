7f3e7d8c0ff00cb9a138160159d84005b10b77cd
Shay Doherty <shaydoherty@mac.home>
2025-02-25 12:31:43 +0000

    add three data buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,33 @@
-resource "aws_s3_bucket" "bucket"{
+resource "aws_s3_bucket" "code_bucket"{
+    bucket_prefix = "data-squid-code-"
+}
+resource "aws_s3_object" "lambda_code"{
+    bucket = aws_s3_bucket.code_bucket.bucket
+    key = "${each.key}/function.zip"
+    source = "${path.module}/../packages/${each.key}/function.zip"
+    for_each = toset([var.extract_lambda])#, var.transform_lambda, var.load_lamabda])
+    etag = filemd5("${path.module}/../packages/${each.key}/function.zip")
+
+}
+
+# resource "aws_s3_object" "lambda_code"{
+#     bucket = aws_s3_bucket.code_bucket.bucket
+#     key = "${each.key}/function.zip"
+#     source = "${path.module}/../packages/${each.key}/function.zip"
+#     for_each = toset([var.extract_lambda, var.transform_lambda, var.load_lamabda])
+#     etag = filemd5("${path.module}/../packages/${each.key}/function.zip")
+    
+# }
+
+resource "aws_s3_bucket" "ingest_bucket"{
+    bucket_prefix = "data-squid-ingest-bucket-"
+}
+resource "aws_s3_bucket" "transform_bucket"{
+    bucket_prefix = "data-squid-transform-bucket-"
+
+}
+
+resource "aws_s3_bucket" "load_bucket"{
+    bucket_prefix = "data-squid-load-bucket-"
 
 }
