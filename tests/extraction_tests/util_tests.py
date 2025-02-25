e0d61bc667e6f33b55fecf58ee1f5307f8422df7
Liam <liambiggar@gmail.com>
2025-02-25 12:25:25 +0000

    adds collect credentials, connect to database and check for data functions

--- tests/extraction_tests/util_tests.py
+++ tests/extraction_tests/util_tests.py
@@ -1,1 +1,70 @@
-from
+from utils.extraction_utils.lambda_utils import collect_credentials_from_AWS, connection_to_database, check_for_data
+from moto import mock_aws
+import boto3
+import pytest
+import os
+import json
+
+@pytest.fixture(scope="function", autouse=False)
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+class TestCollectCredentialsFromAWS:
+    
+    # The function obtains database access keys from AWS Secrets Manager
+    def test_collect_credentials_returns_username_and_password(self):
+        with mock_aws():
+            sm_client = boto3.client('secretsmanager')
+            secret_value = {'user_id': 'test_user', 'password': 'test_password'}
+            sm_client.create_secret(Name='Test_Name', SecretString=json.dumps(secret_value))
+
+            response = collect_credentials_from_AWS(sm_client, secret_id='Test_Name')
+
+            assert response['user_id'] == 'test_user'
+            assert response['password'] == 'test_password'
+
+
+class TestConnectionToDatabase:
+
+    # Use the collect credentials to obtain database AWS Secret
+
+    def test_connection_to_database_can_run_query(self):
+        db = connection_to_database()
+        query = 'SELECT * FROM payment LIMIT 2;'
+        result = db.run(query)
+
+        assert type(result) == list
+        assert len(result) == 2
+
+            
+
+class TestCheckForData:
+    
+    def test_check_for_data_returns_false_if_bucket_empty(self):
+        with mock_aws():
+            #create mock s3 bucket
+            s3_client = boto3.client('s3')
+            #function checks for data in mock s3 bucket
+            s3_client.create_bucket(Bucket='TestBucket', CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
+            # call check for data
+            result = check_for_data(s3_client, bucket_name='TestBucket')
+    
+            assert result == False
+
+
+    def test_check_for_data_returns_true_if_bucket_occupied(self):
+        with mock_aws():
+            #create mock s3 bucket
+            s3_client = boto3.client('s3')
+            #function checks for data in mock s3 bucket
+            s3_client.create_bucket(Bucket='TestBucket', CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
+            #add data to mock s3 bucket
+            s3_client.put_object(Body='Test.txt', Bucket='TestBucket', Key='TestKey')
+            # call check for data
+            result = check_for_data(s3_client, bucket_name='TestBucket')
+            assert result == True
--- utils/extraction_utils/lambda_utils.py
+++ utils/extraction_utils/lambda_utils.py
@@ -1,1 +1,36 @@
 from pg8000.native import Connection
+import boto3
+import json
+
+def collect_credentials_from_AWS(sm_client, secret_id):
+
+    response = sm_client.get_secret_value(SecretId=secret_id)
+    response_json = json.loads(response["SecretString"])
+
+    return response_json
+
+
+
+def connection_to_database():
+
+    sm_client = boto3.client('secretsmanager')
+    secret_id = 'arn:aws:secretsmanager:eu-west-2:195275662632:secret:totesys_database-RBM0fV'
+
+    response = collect_credentials_from_AWS(sm_client, secret_id)
+
+    user = response['username']
+    password = response['password']
+    database = response['dbname']
+    host = response['host']
+    port = response['port']
+
+    return Connection(user=user, password=password, database=database, host=host, port=port)
+
+
+
+def check_for_data(s3_client, bucket_name):
+    ''' This checks for presence of data in S3 Ingestion Bucket'''
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+    print(response)
+    return False if response['KeyCount'] < 1 else True
+
