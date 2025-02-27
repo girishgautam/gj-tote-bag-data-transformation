eae5dd838080ee473af6ddfd04af1e2bb4a3f5f5
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-02-27 15:43:19 +0000

    fix ci/cd - attempt 1 - use git actions absolute path

--- bin/archive_dependencies.sh
+++ bin/archive_dependencies.sh
@@ -5,4 +5,4 @@ rm -rf ../dependencies
 mkdir -p ../dependencies/python
 pip install -r ../requirements-lambda.txt -t ../dependencies/python
 mkdir -p ../packages/dependencies/
-zip ../packages/dependencies/dependencies.zip -r ../dependencies/python
+zip /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/terraform/../packages/dependencies/dependencies.zip -r ../dependencies/python
