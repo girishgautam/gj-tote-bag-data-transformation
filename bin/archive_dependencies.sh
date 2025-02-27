899c20cd5566f12dac0ee582639717c08d76f2fe
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-02-27 15:55:16 +0000

    fix ci/cd - attempt 3 - change to absolute paths

--- bin/archive_dependencies.sh
+++ bin/archive_dependencies.sh
@@ -5,7 +5,7 @@ rm -rf ../dependencies
 mkdir -p ../dependencies/python
 pip install -r ../requirements-lambda.txt -t ../dependencies/python
 mkdir -p ../packages/dependencies/
-zip /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/terraform/../packages/dependencies/dependencies.zip -r ../dependencies/python
-ls -l /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/terraform/../packages/dependencies/dependencies.zip
-chmod +r /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/terraform/../packages/dependencies/dependencies.zip
+zip /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/packages/dependencies/dependencies.zip -r ../dependencies/python
+ls -l /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/packages/dependencies/dependencies.zip
+chmod +r /home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/packages/dependencies/dependencies.zip
 
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -41,7 +41,7 @@ resource "aws_s3_object" "lambda_dependencies" {
     bucket = aws_s3_bucket.code_bucket.bucket
     key = "dependencies/${var.dependencies_zip_filename}"
     # source = "${path.module}/../packages/dependencies/dependencies.zip"
-    source = "/home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/terraform/../packages/dependencies/dependencies.zip"
+    source = abspath("/home/runner/work/de-tote-bag-data-transformation/de-tote-bag-data-transformation/packages/dependencies/dependencies.zip")
 #   When expanding the plan for aws_s3_object.lambda_dependencies to include new values learned so far during apply, provider "registry.terraform.io/hashicorp/aws" produced an
 #   invalid new value for .etag: was cty.StringVal("9c0a09b84ea412a1c5efe856c45ad8d1"), but now cty.StringVal("7e96ae7528f3817c03205d1f9407c564").
 #  
