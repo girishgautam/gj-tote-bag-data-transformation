96bbfd2bad991fe57307466909d712be8bd9a695
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-02-27 15:08:51 +0000

    fix ci/cd - check packages/dependencies/ exists

--- bin/archive_dependencies.sh
+++ bin/archive_dependencies.sh
@@ -4,4 +4,5 @@ set -e
 rm -rf ../dependencies
 mkdir -p ../dependencies/python
 pip install -r ../requirements-lambda.txt -t ../dependencies/python
+mkdir -p ../packages/dependencies/
 zip ../packages/dependencies/dependencies.zip -r ../dependencies/python
