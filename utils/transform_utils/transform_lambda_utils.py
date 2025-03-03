9f5e9b1648f628dfe0c5202b9ddba9249c56e802
girish joshi <girishgautam@yahoo.com>
2025-03-03 10:47:16 +0000

    will add convert_to_json_from_s3 function along with dim_design function

--- utils/transform_utils/transform_lambda_utils.py
+++ utils/transform_utils/transform_lambda_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import io
 
 
-def get_file(table, bucket_name):
+def convert_json_to_df_from_s3(table, bucket_name):
     s3_client = boto3.client('s3')
     last_extracted_obj = s3_client.get_object(Bucket=bucket_name, Key=f'{table}/last_extracted.txt')
     last_extracted_time = last_extracted_obj['Body'].read().decode('utf-8')
@@ -16,6 +16,15 @@ def get_file(table, bucket_name):
 
 # bucket_name = "data-squid-ingest-bucket-20250225123034817500000001"
 
-# df_sales_order = get_file('sales_order', bucket_name)
+# df_design = convert_json_to_df_from_s3('design', bucket_name)
 
-# print(df_sales_order.head(2))
+
+def dim_design(df):
+
+    dim_design = df[['design_id', 'design_name', 'file_location', 'file_name']]
+
+    # dim_design = dim_design.set_index('design_id')
+
+    return dim_design
+
+print(dim_design(df_design))
