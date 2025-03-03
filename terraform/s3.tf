6ecf2831c326e49aab186c926202c5f8c49416af
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-03-03 14:32:45 +0000

    Debug lambda layers - archive dependencies in a python folder

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -23,11 +23,11 @@ resource "aws_s3_bucket" "transform_bucket"{
 
 
 
-resource "aws_s3_object" "lambda_dependencies" {
-    bucket = aws_s3_bucket.code_bucket.bucket
-    key = "dependencies/${var.dependencies_zip_filename}"
-    source = data.archive_file.dependencies.output_path
-}  
+# resource "aws_s3_object" "lambda_dependencies" {
+#     bucket = aws_s3_bucket.code_bucket.bucket
+#     key = "dependencies/${var.dependencies_zip_filename}"
+#     source = data.archive_file.dependencies.output_path
+# }
 # resource "aws_s3_object" "transform_lambda" {
 #     bucket = aws_s3_bucket.code_bucket.bucket
 #     key = "${var.transform_lambda}/${var.extraction_utils_zip_filename}"
