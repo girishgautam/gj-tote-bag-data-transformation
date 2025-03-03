4dac4d32b52619f7301271ab012d9ffbd5da2feb
girish joshi <girishgautam@yahoo.com>
2025-03-03 13:50:14 +0000

    add 2 dim util functions, dim_staff and dim_design along with its tests

--- utils/transform_utils/transform_lambda_utils.py
+++ utils/transform_utils/transform_lambda_utils.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import pandas as pd
 import io
+from utils.extraction_utils.lambda_utils import get_s3_bucket_name
 
 
 def convert_json_to_df_from_s3(table, bucket_name):
@@ -14,12 +15,35 @@ def convert_json_to_df_from_s3(table, bucket_name):
     df = pd.read_json(json_file_io)
     return df
 
-# bucket_name = "data-squid-ingest-bucket-20250225123034817500000001"
+# bucket_name = get_s3_bucket_name("data-squid-ingest-bucket-")
 
-# df_design = convert_json_to_df_from_s3('design', bucket_name)
+# df_staff = convert_json_to_df_from_s3('staff', bucket_name)
+# df_department = convert_json_to_df_from_s3('department', bucket_name)
 
 
 def dim_design(df):
+    """
+    Extracts a subset of columns from the input Design DataFrame to create the `dim_design` DataFrame.
+
+    This function selects the following columns from the input DataFrame:
+    - 'design_id'
+    - 'design_name'
+    - 'file_location'
+    - 'file_name'
+
+    The resulting DataFrame is used for design-related information and can be further processed or analyzed.
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The input DataFrame containing design-related data.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        A DataFrame containing the columns: 'design_id', 'design_name',
+        'file_location', and 'file_name'.
+    """
 
     dim_design = df[['design_id', 'design_name', 'file_location', 'file_name']]
 
@@ -27,4 +51,31 @@ def dim_design(df):
 
     return dim_design
 
-print(dim_design(df_design))
+
+def dim_staff(df_1, df_2):
+    """
+    Merges the staff and department tables to create a dimensional staff DataFrame.
+
+    Parameters:
+    -----------
+    staff_df : pandas.DataFrame
+        The staff table containing staff-related information.
+    department_df : pandas.DataFrame
+        The department table containing department-related information.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        A DataFrame containing the merged data with the following columns:
+        - 'first_name'
+        - 'last_name'
+        - 'department_name'
+        - 'location'
+        - 'email_address'
+    """
+    try:
+        dim_staff_df = pd.merge(df_1, df_2, on='department_id', how='inner')
+        dim_staff_df = dim_staff_df[['first_name', 'last_name', 'department_name', 'location', 'email_address']]
+        return dim_staff_df
+    except KeyError:
+        raise
