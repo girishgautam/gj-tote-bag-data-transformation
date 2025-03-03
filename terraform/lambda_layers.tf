d07b75caa35df3033118d658687d859c7a6734de
Shay Doherty <shaydoherty@mac.home>
2025-03-03 11:46:06 +0000

    debug

--- terraform/lambda_layers.tf
+++ terraform/lambda_layers.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "extraction_utils_layer" {
   # s3_bucket = aws_s3_bucket.code_bucket.bucket
   # s3_key = aws_s3_object.extraction_utils.key
   filename = "${path.module}/../packages/extraction_utils/utils.zip"
-  source_code_hash = filebase64sha256("${path.module}/../packages/extraction_utils/utils.zip")
+  #source_code_hash = filebase64sha256("${path.module}/../packages/extraction_utils/utils.zip")
 }
 
 resource "aws_lambda_layer_version" "dependencies_layer" {
