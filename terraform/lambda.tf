12fd2d30b240c157402309b6b897dbf271fc59a3
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-03-04 15:06:51 +0000

    Terraform transform lambda and lambda layers. Add pandas library.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -27,6 +27,35 @@ resource "aws_lambda_function" "extract_lambda" {
   }
 }
 
+#Archived code for transform lambda
+data "archive_file" "transform_lambda"{
+    type = "zip"
+    output_file_mode = "0666"
+    source_file = "${path.module}/../src/transform_lambda/main.py"
+    output_path = "${path.module}/../packages/${var.transform_lambda}/function.zip"
+}
+
+# Create transform lambda
+resource "aws_lambda_function" "transform_lambda" {
+    role = aws_iam_role.lambda_role.arn
+    function_name = var.transform_lambda
+    layers = [
+      aws_lambda_layer_version.transformation_utils_layer.arn,
+      aws_lambda_layer_version.dependencies_layer.arn
+    ]
+    filename = data.archive_file.transform_lambda.output_path
+    source_code_hash = filebase64sha256(data.archive_file.transform_lambda.output_path)
+    handler = "main.lambda_handler"
+    timeout = 900
+    runtime = "python3.12"
+
+    environment {
+    variables = {
+      BUCKET_TRANSFORM = aws_s3_bucket.transform_bucket.bucket
+    }
+  }
+}
+
 # data "archive_file" "extraction_utils"{
 #     type = "zip"
 #     output_file_mode = "0666"
