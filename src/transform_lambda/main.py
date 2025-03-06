0481fceb8b9f0c6ca3df6b1a995e5900e76ec186
Liam <liambiggar@gmail.com>
2025-03-06 14:34:11 +0000

    lambda handler completed with tests

--- src/transform_lambda/main.py
+++ src/transform_lambda/main.py
@@ -1,6 +1,8 @@
 import boto3
 import json
-import datetime
+from datetime import datetime
+import logging
+from botocore.exceptions import ClientError, NoCredentialsError
 from utils.lambda_utils import (convert_json_to_df_from_s3, 
                                 dim_design, dim_date, 
                                 dim_counterparty, dim_currency, 
@@ -10,37 +12,67 @@ from utils.lambda_utils import (convert_json_to_df_from_s3,
                                 get_s3_bucket_name, check_for_data)
 
 
+logger = logging.getLogger()
+logger.setLevel('INFO')
+
 def lambda_handler(event, context):
-    s3_client = boto3.client('s3')
+    try:
+        s3_client = boto3.client('s3')
+    except NoCredentialsError:
+        logging.error("AWS credentials not found. Unable to create S3 client")
+        return {"result": "Failure", "error": "AWS credentials not found."}
+    except ClientError as e:
+        logging.error(f"Error creating S3 client: {e}")
+        return {"result": "Failure", "error": "Error creating S3 client"}
+    
     timestamp = datetime.now()
     timestamp_for_filename = timestamp.strftime("%Y/%m/%d/%H:%M")
     timestamp_for_last_extracted = timestamp_for_filename.encode("utf-8")
     ingestion_bucket_name = event['Records'][0]['s3']['bucket']['name']
-    report_file = event['Records'][0]['object']['key']
+    report_file = event['Records'][0]['s3']['object']['key']
     transform_bucket_name = get_s3_bucket_name('data-squid-transform')
 
     #These 4 variables are created for the purpose of the dim tables which require 2 dataframes to be created
     #If these paired values (address_df, counterparty_df), (department_df, staff_df) are NOT NONE eg they have recently been ingested
     #Then a new dim table (either dim_counterparty or dim_staff or both) will be created and stored in the transform bucket
     address_df = None
+    address_df_exists = False
     counterparty_df = None
+    counterparty_df_exists = False
 
-    department_df = None 
+    department_df = None
+    department_df_exists = False
     staff_df = None
+    staff_df_exists = False
+
+    fact_sales_order_table_created = False
+    transformed_tables = []
 
     # Checks for presence of data in transform bucket, as if no data present we need to create dim_date table
     # If data is present, we do not need to create this table again as the dates will not change and are for a set period
     if check_for_data(s3_client, transform_bucket_name) == False:
         dim_date_tab = dim_date()
         parquet_file = dataframe_to_parquet(dim_date_tab)
-        upload_to_s3(data=parquet_file, bucket_name=transform_bucket_name, object_name="dim_date/dim_date.pqt")
+        filename = create_filename_for_parquet("dim_date", timestamp_for_filename)
+        upload_to_s3(data=parquet_file, bucket_name=transform_bucket_name, object_name=filename)
+        s3_client.put_object(
+                Bucket=transform_bucket_name,
+                Key="dim_date/last_transformed.txt",
+                Body=timestamp_for_last_extracted,
+            )
+        transformed_tables.append('dim_date')
 
     tables = extract_tablenames(s3_client, ingestion_bucket_name, report_file)
-
+    print(tables, '<<<<<<< TABLES')
     # Iterates through all of the tables that have recently been ingested by the ingestion lambda function
     # and stores the converted dataframe to parquet file in the transform bucket
     for table in tables:
-        dataframe = convert_json_to_df_from_s3(table, ingestion_bucket_name)
+        try:
+            dataframe = convert_json_to_df_from_s3(table, ingestion_bucket_name)
+        except ClientError as e:
+            logger.warning('Invalid table name')
+        
+
         if table == 'design':
             dim_design_tab = dim_design(dataframe)
             parquet_file = dataframe_to_parquet(dim_design_tab)
@@ -51,6 +83,7 @@ def lambda_handler(event, context):
                 Key="dim_design/last_transformed.txt",
                 Body=timestamp_for_last_extracted,
             )
+            transformed_tables.append('dim_design')
         elif table == 'address':
             dim_location_tab = dim_location(dataframe)
             parquet_file = dataframe_to_parquet(dim_location_tab)
@@ -61,7 +94,9 @@ def lambda_handler(event, context):
                 Key="dim_location/last_transformed.txt",
                 Body=timestamp_for_last_extracted,
             )
+            transformed_tables.append('dim_location')
             address_df = dataframe
+            address_df_exists = True
         elif table == 'currency':
             dim_currency_tab = dim_currency(dataframe)
             parquet_file = dataframe_to_parquet(dim_currency_tab)
@@ -72,6 +107,7 @@ def lambda_handler(event, context):
                 Key="dim_currency/last_transformed.txt",
                 Body=timestamp_for_last_extracted,
             )
+            transformed_tables.append('dim_currency')
         elif table == 'sales_order':
             fact_sales_tab = fact_sales_order(dataframe)
             parquet_file = dataframe_to_parquet(fact_sales_tab)
@@ -82,14 +118,20 @@ def lambda_handler(event, context):
                 Key="fact_sales_order/last_transformed.txt",
                 Body=timestamp_for_last_extracted,
             )
+            fact_sales_order_table_created = True
         elif table == 'counterparty':
             counterparty_df = dataframe
+            counterparty_df_exists = True
         elif table == 'department':
             department_df = dataframe
+            department_df_exists = True
         elif table == 'staff':
             staff_df = dataframe
+            staff_df_exists = True
+        
+
     
-    if address_df and counterparty_df:
+    if address_df_exists and counterparty_df_exists:
         dim_counterparty_tab = dim_counterparty(address_df, counterparty_df)
         parquet_file = dataframe_to_parquet(dim_counterparty_tab)
         filename = create_filename_for_parquet("dim_counterparty", timestamp_for_filename)
@@ -99,7 +141,8 @@ def lambda_handler(event, context):
             Key="dim_counterparty/last_transformed.txt",
             Body=timestamp_for_last_extracted,
         )
-    if department_df and staff_df:
+        transformed_tables.append('dim_counterparty')
+    if department_df_exists and staff_df_exists:
         dim_staff_tab = dim_staff(department_df, staff_df)
         parquet_file = dataframe_to_parquet(dim_staff_tab)
         filename = create_filename_for_parquet("dim_staff", timestamp_for_filename)
@@ -109,6 +152,43 @@ def lambda_handler(event, context):
             Key="dim_staff/last_transformed.txt",
             Body=timestamp_for_last_extracted,
         )
+        transformed_tables.append('dim_staff')
+
+    if fact_sales_order_table_created:
+        transformed_tables.append('fact_sales_order')
+
+    print(address_df, '<<<<<<<< ADDRESS DF')
+
+    # May not be necessary to have the if statement, will only run without tables...
+    # ..if there is an error in activation of the Lambda
+    if transformed_tables:
+
+        report = {
+            "status": "Success",
+            "transformed_tables": transformed_tables,
+        }
+        report_file_name = f"reports/{datetime.now().isoformat()}_success.json"
+
+        s3_client.put_object(
+            Body=json.dumps(report, indent=4),
+            Bucket=transform_bucket_name,
+            Key=report_file_name,
+        )
+
+        logger.info(
+            f"Transformation successful. Report stored in\
+                S3: s3://{transform_bucket_name}/{report_file_name}"
+        )
+
+        return {
+            "result": "Success",
+            "report_file": f"s3://{transform_bucket_name}/{report_file_name}",
+        }
+    else:
+        logger.warning('Lambda was called without valid tables. Extraction report should not have been created')
+
+        return 'Lambda was called without valid tables. Extraction report should not have been created'
+
 
 
 
--- tests/transform_tests/test_transform_utils.py
+++ tests/transform_tests/test_transform_utils.py
@@ -13,8 +13,8 @@ from utils.lambda_utils import (
     create_filename,
     upload_to_s3,
 )
-from src.transform_lambda.main import extract_tablenames
-
+from src.transform_lambda.main import extract_tablenames, lambda_handler
+from unittest.mock import patch
 import boto3
 import pytest
 import os
@@ -627,3 +627,116 @@ class TestExtractTableNamesFromReport:
             tables = extract_tablenames(s3_client, "TestBucket", "test_report.json")
             assert tables[0] == 'test_table1'
             assert tables[1] == 'test_table2'
+
+
+class TestLambdaHandler:
+    @patch("utils.lambda_utils.get_s3_bucket_name")
+    @patch("utils.lambda_utils.check_for_data")
+    @patch("src.transform_lambda.main.extract_tablenames")
+    def test_lambda_handler_erroneously_called_returns_warning_message(self, mock_extract_tablenames, mock_check_for_data, mock_get_s3_bucket_name):
+        mock_check_for_data.return_value = True
+        mock_extract_tablenames.return_value = ['fake_table']
+        mock_event = {'Records': [{'s3': {'bucket': {'name': 'TestBucket'}, 'object': {'key': 'test_report.json'}}}]}
+
+
+        mock_get_s3_bucket_name.return_value = 'TestBucket'
+
+        result = lambda_handler(mock_event, {})
+
+        assert result == 'Lambda was called without valid tables. Extraction report should not have been created'
+
+    @patch("utils.lambda_utils.get_s3_bucket_name")
+    @patch("utils.lambda_utils.check_for_data")
+    @patch("src.transform_lambda.main.extract_tablenames")
+    def test_lambda_handler_called_with_empty_list_returns_warning_message(self, mock_extract_tablenames, mock_check_for_data, mock_get_s3_bucket_name):
+        mock_check_for_data.return_value = True
+        mock_extract_tablenames.return_value = []
+        mock_event = {'Records': [{'s3': {'bucket': {'name': 'TestBucket'}, 'object': {'key': 'test_report.json'}}}]}
+
+
+        mock_get_s3_bucket_name.return_value = 'TestBucket'
+
+        result = lambda_handler(mock_event, {})
+
+        assert result == 'Lambda was called without valid tables. Extraction report should not have been created'
+
+    def test_lambda_handler_success_with_valid_trigger(self):
+        
+        rows = [
+            (
+                1,
+                "6826 Herzog Via",
+                None,
+                "Avon",
+                "New Patienceburgh",
+                "28441",
+                "Turkey",
+                "1803 637401",
+            ),
+            (
+                2,
+                "179 Alexie Cliffs",
+                None,
+                None,
+                "Aliso Viejo",
+                "99305-7380",
+                "San Marino",
+                "9621 880720",
+            ),
+        ]
+        columns = [
+            "address_id",
+            "address_line_1",
+            "address_line_2",
+            "district",
+            "city",
+            "postal_code",
+            "country",
+            "phone",
+        ]
+
+
+        with mock_aws():
+            s3_client = boto3.client("s3")
+            s3_client.create_bucket(
+                Bucket="TestIngestBucket",
+                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+            )
+
+            mock_event = {'Records': [{'s3': {'bucket': {'name': 'TestIngestBucket'}, 'object': {'key': 'reports/test_report.json'}}}]}
+
+            s3_client.create_bucket(
+                Bucket="data-squid-transform-test",
+                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+            )
+            
+            body_for_report = {
+                "status": "Success",
+                "updated_tables": ["address"]
+            }
+            address_json = format_data_to_json(rows, columns)
+            timestamp_for_filename = datetime.now().strftime("%Y/%m/%d/%H:%M")
+            timestamp_for_last_extracted = timestamp_for_filename.encode("utf-8")
+            s3_client.put_object(
+                Bucket="TestIngestBucket",
+                Key=f"address/last_extracted.txt",
+                Body=timestamp_for_last_extracted,
+            )
+            filename = create_filename(
+                table_name="address", time=timestamp_for_filename
+            )
+            
+            upload_to_s3(data=address_json, bucket_name="TestIngestBucket", object_name=filename)
+            
+            s3_client.put_object(
+                Bucket="TestIngestBucket",
+                Key=f"reports/test_report.json",
+                Body=json.dumps(body_for_report, indent=4),
+            )
+            
+        
+            result = lambda_handler(mock_event, {})
+
+            assert result["result"] == "Success"
+            assert "s3://data-squid-transform-test" in result["report_file"]
+    
