3e9dabb9fec2e2e188cec17c94133499d5db4416
Nicolas Tolksdorf <tolksdorf@gmail.com>
2025-03-10 09:47:57 +0000

    Turn-off scheduled invocations on extract lambda

--- terraform/events.tf
+++ terraform/events.tf
@@ -1,25 +1,25 @@
-resource "aws_cloudwatch_event_rule" "scheduler" {
-  # This should set up a scheduler that will trigger the Lambda
-  # Careful! other things may need to be set up as well
-  name = "trigger-extraction-lambda"
-  schedule_expression = "rate(5 minutes)"
-  description = "trigger extraction lambda"
-}
+# resource "aws_cloudwatch_event_rule" "scheduler" {
+#   # This should set up a scheduler that will trigger the Lambda
+#   # Careful! other things may need to be set up as well
+#   name = "trigger-extraction-lambda"
+#   schedule_expression = "rate(5 minutes)"
+#   description = "trigger extraction lambda"
+# }
 
-resource "aws_cloudwatch_event_target" "sns" {
-  rule      = aws_cloudwatch_event_rule.scheduler.name
-  target_id = "InvokeExtractionLambda"
-  # target id not required just a label to help identify target
+# resource "aws_cloudwatch_event_target" "sns" {
+#   rule      = aws_cloudwatch_event_rule.scheduler.name
+#   target_id = "InvokeExtractionLambda"
+#   # target id not required just a label to help identify target
 
-  arn       = aws_lambda_function.extract_lambda.arn
-}
+#   arn       = aws_lambda_function.extract_lambda.arn
+# }
 
-resource "aws_lambda_permission" "event_permissions" {
-  function_name = aws_lambda_function.extract_lambda.function_name
-  action = "lambda:InvokeFunction"
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.scheduler.arn
-}
+# resource "aws_lambda_permission" "event_permissions" {
+#   function_name = aws_lambda_function.extract_lambda.function_name
+#   action = "lambda:InvokeFunction"
+#   principal = "events.amazonaws.com"
+#   source_arn = aws_cloudwatch_event_rule.scheduler.arn
+# }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.ingest_bucket.id
